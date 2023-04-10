a7b83ec42f91babf4aa1a56cc1567a4ec1dbae85
vinodh1988 <k.c.vinodh@hotmail.com>
2023-04-10 11:11:04 +0530

    third-commit

---
+++ object.js
@@ -0,0 +1,9 @@
+/*class person {
+    sno:number
+    name:string
+    city:string
+}*/
+var people = [
+    { sno: 1, name: "Rahul", city: "Chennai" },
+    { sno: 1, name: "Rahul", city: "Chennai" }
+];
---
+++ object.ts
@@ -0,0 +1,15 @@
+/*class person {
+    sno:number
+    name:string
+    city:string
+}*/
+
+interface person {
+    sno:number
+    name:string
+    city:string
+}
+const people:person[] = [
+    {sno:1,name:"Rahul",city:"Chennai"},
+    {sno:1,name:"Rahul",city:"Chennai"}
+]
