3364a905e0fb77bb1fa4d58636a971d310ded93d
vinodh1988 <k.c.vinodh@hotmail.com>
2023-04-10 10:57:19 +0530

    typescript-commit

---
+++ functions.js
@@ -0,0 +1,11 @@
+/*
+  number, string, boolean
+
+*/
+function sample(a, b) {
+    return a + b;
+}
+//sample() //error
+//sample(12) //error
+//sample(12,"Raj") //error
+console.log(sample(23, 43.34));
---
+++ functions.ts
@@ -0,0 +1,13 @@
+/*
+  number, string, boolean
+
+*/
+
+function sample(a:number,b:number):number{
+    return a+b;
+}
+
+//sample() //error
+//sample(12) //error
+//sample(12,"Raj") //error
+console.log(sample(23,43.34))
